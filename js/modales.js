60767c013bec6296a0d2b08c77df0d536528821f
Jesús R Peinado <jesusrpeinado@gmail.com>
2014-12-09 09:18:51 +0100

    Style Guide Modal

--- js/modales.js
+++ js/modales.js
@@ -8,45 +8,17 @@
 
 (function ($) {
   $(document).ready(function () {
-    // INIT $.ready() ////////////////////////
-
-    // Preparamos algunos modales
-    var $modalDrupalBase = $('.emergya-drupal-base .modal-content').easyModal();
-
-    var contactFormCopy_a = $('<div class="form-contact_a form-contact"></div>').append($('.emergya-drupal-base .contact-form')).addClass('modal-content hidden');
-
-    var $aClose = $('<a class="close" href="#">Close</a>');
-    $aClose.clone().appendTo(contactFormCopy_a);
-
-    var $body = $('body');
-    contactFormCopy_a.appendTo($body);
-    contactFormCopy_a.easyModal();
-
-    // Handlers para abrir los modales.
-    $('.solution .content .more').on('click', function (e) {
-      $(this).parent().siblings('.modal-content').trigger('openModal');
-      e.preventDefault();
-    });
-
-    // Control del compotamiento dentro de los modales.
-    $('.show-form').on('click', function (e) {
-      e.preventDefault();
-      var $show_form = $(this).parent().parent();
-      $(this).parent().trigger('closeModal');
-      
-      if ($show_form.hasClass('emergya-drupal-base')) {
-        contactFormCopy_a.trigger('openModal');
-      }
-    });
-
-    var $marca = $('.marca .more');
-    $marca.siblings('.modal-content').easyModal();
-    // Handlers para abrir los modales.
-    $marca.on('click', function (e) {
-      $(this).siblings('.modal-content').trigger('openModal');
-      e.preventDefault();
-    });
-
-    // END $.ready() ////////////////////////
+      $('body').append('<div id="style-guide-modal" class="generic-modal"><div class="modal-content"></div></div>');
+      $('#style-guide-modal .modal-content').easyModal();      
+      $('#style-guide-modal .modal-content').load({url: "http://dev.davinci.es/admin/appearance/styleguide/da_vinci"});
+      $('.style-guide-modal-fire').on('click',function(e){
+        e.preventDefault();
+        $.get("./admin/appearance/styleguide/da_vinci").then(function(data){
+          $('#style-guide-modal .modal-content').html(data);
+        });
+        $('#style-guide-modal .modal-content').html($('#style-guide-modal .modal-content #block-system-main'));
+        $('#style-guide-modal .modal-content').append('<a class="close" href="#">Close</a>');
+        $('#style-guide-modal .modal-content').trigger('openModal');
+      });
   });
 })(jQuery);
