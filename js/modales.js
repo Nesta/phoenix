f75de5cc42a2e03b4f0be14ac90c616c3d8bf272
Jesús R Peinado <jesusrpeinado@gmail.com>
2014-12-09 22:08:57 +0100

    Fixing the Style Guide Modal

--- js/modales.js
+++ js/modales.js
@@ -10,14 +10,13 @@
   $(document).ready(function () {
       $('body').append('<div id="style-guide-modal" class="generic-modal"><div class="modal-content"></div></div>');
       $('#style-guide-modal .modal-content').easyModal();      
-      $('#style-guide-modal .modal-content').load({url: "http://dev.davinci.es/admin/appearance/styleguide/da_vinci"});
       $('.style-guide-modal-fire').on('click',function(e){
         e.preventDefault();
         $.get("./admin/appearance/styleguide/da_vinci").then(function(data){
-          $('#style-guide-modal .modal-content').html(data);
+          $('#style-guide-modal .modal-content').html($("#block-system-main",data));
+          $('#style-guide-modal .modal-content').append('<a class="close" href="#">Close</a>');
+          $('#style-guide-modal .modal-content').css({'top':'10%','left':'10%','width':'80%','height':'80%','overflow-y':'auto','margin-left':'1px','margin-top':'1px'});
         });
-        $('#style-guide-modal .modal-content').html($('#style-guide-modal .modal-content #block-system-main'));
-        $('#style-guide-modal .modal-content').append('<a class="close" href="#">Close</a>');
         $('#style-guide-modal .modal-content').trigger('openModal');
       });
   });
