e7801628c6b0f3ec314443e2c410fd014460160b
Jose Maria Fernandez <jmfernandez@emergya.com>
2014-12-19 13:20:45 +0100

    Masonry and other changes

--- preprocess/template.preprocess.view.php
+++ preprocess/template.preprocess.view.php
@@ -10,14 +10,25 @@
 /**
  * Implements template_preprocess_views_view().
  */
+// Creamos una función donde definimos la vista a la que le aplicaremos el plugin Masonry
 function da_vinci_preprocess_views_view(&$vars) {
-  
-  /* EXAMPLE
-  if ($vars['view']->name == 'View_Name'){
+  // Definimos el nombre de la vista
+  if ($vars['view']->name == 'infinite_list'){
+
+      // Definimos la ruta a los plugins necesarios a cargar ¡SOLO! en esta vista.
       $theme_path = drupal_get_path('theme', 'da_vinci');
-      //drupal_add_js($theme_path . '/js/plugins/plugin.js');
+
+        // Plugin masonry: http://masonry.desandro.com/masonry.pkgd.min.js
+        drupal_add_js($theme_path . '/js/plugins/masonry.js');
+
+        // Plugin classie: https://github.com/desandro/classie/blob/master/classie.js
+        drupal_add_js($theme_path . '/js/plugins/classie.js');
+
+        // Plugin imageload: http://imagesloaded.desandro.com/imagesloaded.pkgd.min.js
+        drupal_add_js($theme_path . '/js/plugins/imageload.js');
+
+        // Plugin masonry-view.js: http://codepen.io/desandro/pen/IBJeq
+        // Ver masonry-view.js
+        drupal_add_js($theme_path . '/js/masonry-view.js');
   }
-   */
-
 }
-
