b6fed8a7d096ee29aea9fe00d16a7ba4884d9feb
Jesús R Peinado <jesusrpeinado@gmail.com>
2014-12-23 14:08:38 +0100

    JS documentation

--- js/modales.js
+++ js/modales.js
@@ -1,27 +1,25 @@
 /**
- * @file
- * The theme system, which controls the output of Drupal.
+ * @file  
+ * Modal output and behaviour 
  *
- * The theme system allows for nearly all output of the Drupal system to be
- * customized by user themes.
+ * @pre Style Guide Icon setting active
+ * @pre style-guide Module installed
+ * @pre User admin logged in
+ *
+ * We get the content of the style-guide module (through ajax) into the modal window, 
+ * displaying a fixed icon at the right side. 
  */
-
 (function ($) {
   $(document).ready(function () {
-      // Se ha capturado el contenido del modulo styleguide y se ha convertido en una ventana modal,
-      // a la cual podemos acceder mediante el icono "brush" situado de manera fija en el lateral
-      // derecho del portal, sin embargo, es posible acceder a la pagina como tal que nos facilita el
-      // modulo desde la apariencia del tema.
-      $('body').append('<div id="style-guide-modal" class="generic-modal"><div class="modal-content"></div></div>');
-      $('#style-guide-modal .modal-content').easyModal();      
-      $('.style-guide-modal-fire').on('click',function(e){
-        e.preventDefault();
-        $.get("/admin/appearance/styleguide/da_vinci").then(function(data){
-          $('#style-guide-modal .modal-content').html($("#block-system-main",data));
-          $('#style-guide-modal .modal-content').append('<a class="close" href="#">Close</a>');
-          $('#style-guide-modal .modal-content').css({'top':'10%','left':'10%','width':'80%','height':'80%','overflow-y':'auto','margin-left':'1px','margin-top':'1px'});
-        });
-        $('#style-guide-modal .modal-content').trigger('openModal');
+    $('body').append('<div id="style-guide-modal" class="generic-modal"><div class="modal-content"><a class="close" href="#">Close</a></div></div>');
+    $('#style-guide-modal .modal-content').easyModal();      
+    $('.style-guide-modal-fire').on('click',function(e){
+      e.preventDefault();
+      $.get("/admin/appearance/styleguide/da_vinci").then(function(data){
+        $('#style-guide-modal .modal-content').html($("#block-system-main",data));
+        $('#style-guide-modal .modal-content').css({'top':'10%','left':'10%','width':'80%','height':'80%','overflow-y':'auto','margin-left':'1px','margin-top':'1px'});
       });
+      $('#style-guide-modal .modal-content').trigger('openModal');
+    });
   });
 })(jQuery);
