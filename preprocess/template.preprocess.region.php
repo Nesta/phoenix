db828b9c016c03154ec991c7e1c4e961102d90d7
Jesús R Peinado <jesusrpeinado@gmail.com>
2015-01-07 11:05:19 +0100

    PHP cleaning out

--- preprocess/template.preprocess.region.php
+++ preprocess/template.preprocess.region.php
@@ -1,12 +1,8 @@
 <?php
 /**
  * @file
- * The theme system, which controls the output of Drupal.
- *
- * The theme system allows for nearly all output of the Drupal system to be
- * customized by user themes.
+ * Preproccess functions for Regions.
  */
-
 /**
  * Implement template_preprocess_block().
  */
