4dd16b6ae262edd2e8a47a305081899e63064468
Nesta <nguerrero@emergya.com>
2015-01-20 17:11:31 +0100

    Fix Coder

--- theme-settings.php
+++ theme-settings.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * @file
- * Theme setting callbacks for the da_vinci theme.
  *   @param $form: Nested array of form elements that comprise the form.
  *   @param $form_state: A keyed array containing the current state of the form.
- * Implements hook_form_FORM_ID_alter().
+ * Theme setting callbacks for the da_vinci theme.
  *
+ * Implements hook_form_FORM_ID_alter().
  */
 
 function da_vinci_form_system_theme_settings_alter(&$form, &$form_state) {
