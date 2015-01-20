f0d348ca34d00890bd935f62788076c7df9082f5
Nesta <nguerrero@emergya.com>
2015-01-20 11:37:30 +0100

    Fix Coder

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Default theme implementation to display the basic html structure of a single
+ * Default theme implementation to display the basic html structure of a single.
  *
  * Drupal page.
  *
