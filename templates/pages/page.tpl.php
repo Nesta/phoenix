fcb771f90fd11e5d9c346c793d8fe6a5614429f5
Nesta <nguerrero@emergya.com>
2015-01-20 13:26:11 +0100

    Fix Coder

--- templates/pages/page.tpl.php
+++ templates/pages/page.tpl.php
@@ -112,16 +112,14 @@
   <?php endif; ?>
   <?php if ($main_menu || $secondary_menu): ?>
       <div id="navigation"><div class="section container">
-        <?php print theme('links__system_main_menu',
-          array('links' => $main_menu, 'attributes' => 
-            array('id' => 'main-menu', 'class' => 
-            array('links', 'inline', 'clearfix')), 
-            'heading' => t('Main menu'))); ?>
-        <?php print theme('links__system_secondary_menu',
-              array('links' => $secondary_menu,
-              'attributes' => array('id' => 'secondary-menu',
-              'class' => array('links', 'inline', 'clearfix')),
-              'heading' => t('Secondary menu'))); ?>
+        <?php print theme('links__system_main_menu', array('links' => $main_menu, 
+          'attributes' => array('id' => 'main-menu', 'class' => 
+          array('links', 'inline', 'clearfix')), 
+          'heading' => t('Main menu'))); ?>
+        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu,
+          'attributes' => array('id' => 'secondary-menu',
+          'class' => array('links', 'inline', 'clearfix')),
+          'heading' => t('Secondary menu'))); ?>
       </div></div> <!-- /.section, /#navigation -->
   <?php endif; ?>
   <div id="main-content" class="main-content container">
