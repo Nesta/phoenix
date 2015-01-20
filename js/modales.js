5aabe1b4d7a4fb51b0b71b4cdad0ce6eb550b752
Nesta <nguerrero@emergya.com>
2015-01-20 10:39:58 +0100

    Fix Coder

--- js/modales.js
+++ js/modales.js
@@ -1,5 +1,5 @@
 /**
- * @file 
+ * @file
  * Modal output and behaviour.
  *
  * @pre Style Guide Icon setting active
@@ -15,21 +15,21 @@
     /** 
      *  @brief da_vinci_modal
      *  Object Literal for setup and populating modals
-     *  
+     *
      *  @param id, will be the #id of your modal
      *  @param url, will be the target of the ajax content
      *  @param fire, will be the click element for display the modal
      */
     var da_vinci_modal = {
       init: function(id,url,fire){
-        $('body').append('<div id="'+ id +'" class="generic-modal"><div class="modal-content"><a class="close" href="#">Close</a></div></div>');
+        $('body').append('<div id="' + id + '" class="generic-modal"><div class="modal-content"><a class="close" href="#">Close</a></div></div>');
         this.attach(id,url,fire);
       },
       attach: function(id,url,fire){
         p=this;
         $('#'+id+' .modal-content').easyModal();
         $(fire).on('click',function(e){
-          p.onClick(id,url,e);  
+          p.onClick(id,url,e);
         });
       },
       onClick: function(id,url,e){
@@ -44,7 +44,7 @@
         });
       },
       open: function(id){
-        $('#'+id+' .modal-content').trigger('openModal');    
+        $('#'+id+' .modal-content').trigger('openModal');
       }
     };
     var style_guide_modal = da_vinci_modal.init('style-guide-modal','?q=/admin/appearance/styleguide/da_vinci','.style-guide-modal-fire');
