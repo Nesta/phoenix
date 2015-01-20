2dab60aae32cd54fe7eb78b3f850da37b4af218c
Nesta <nguerrero@emergya.com>
2015-01-20 13:38:19 +0100

    Fix Coder

--- templates/pages/page.tpl.php
+++ templates/pages/page.tpl.php
@@ -112,9 +112,11 @@
   <?php endif; ?>
   <?php if ($main_menu || $secondary_menu): ?>
       <div id="navigation"><div class="section container">
-        <?php print theme('links__system_main_menu', array('links' => $main_menu, 
-          'attributes' => array('id' => 'main-menu', 'class' => 
-          array('links', 'inline', 'clearfix')), 
+        <?php print theme('links__system_main_menu', array(
+          'links' => $main_menu,
+          'attributes' => array(
+            'id' => 'main-menu',
+            'class' => array('links', 'inline', 'clearfix')),
           'heading' => t('Main menu'))); ?>
         <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu,
           'attributes' => array('id' => 'secondary-menu',
