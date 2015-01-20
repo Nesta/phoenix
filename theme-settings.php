96253070780cf107d13cd1dd8031ede070981530
Nesta <nguerrero@emergya.com>
2015-01-20 16:30:59 +0100

    Fix Coder

--- theme-settings.php
+++ theme-settings.php
@@ -9,8 +9,9 @@
  * Implements hook_form_FORM_ID_alter().
  *
  * @param $form
+ *   The form.
  * @param $form_state
- *   The form and form state.
+ *   The form state.
  */
 function da_vinci_form_system_theme_settings_alter(&$form, &$form_state) {
 
