131a5d925b65c54800fb66041f9c2bed0f8a584a
Nesta <nguerrero@emergya.com>
2015-01-20 13:12:19 +0100

    Fix Coder

--- templates/pages/page.tpl.php
+++ templates/pages/page.tpl.php
@@ -112,15 +112,15 @@
   <?php endif; ?>
   <?php if ($main_menu || $secondary_menu): ?>
       <div id="navigation"><div class="section container">
-        <?php print theme('links__system_main_menu', 
-              array('links' => $main_menu, 
-                'attributes' => array('id' => 'main-menu', 
-                'class' => array('links', 'inline', 'clearfix')), 
+        <?php print theme('links__system_main_menu',
+              array('links' => $main_menu,
+                'attributes' => array('id' => 'main-menu',
+                'class' => array('links', 'inline', 'clearfix')),
                 'heading' => t('Main menu'))); ?>
-        <?php print theme('links__system_secondary_menu', 
-              array('links' => $secondary_menu, 
-              'attributes' => array('id' => 'secondary-menu', 
-              'class' => array('links', 'inline', 'clearfix')), 
+        <?php print theme('links__system_secondary_menu',
+              array('links' => $secondary_menu,
+              'attributes' => array('id' => 'secondary-menu',
+              'class' => array('links', 'inline', 'clearfix')),
               'heading' => t('Secondary menu'))); ?>
       </div></div> <!-- /.section, /#navigation -->
   <?php endif; ?>
