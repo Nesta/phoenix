741314b01097927c3a6e058464fac46bf6d4f429
Carmen García <mcgarcia@emergya.com>
2017-03-29 09:33:20 +0200

    Initial files for STARTERKIT

---
+++ STARTERKIT/js/mytheme.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * My theme Custom Code of the javascript behaviour.
+ */
+
+(function ($) {
+  Drupal.behaviors.da_vinciTheme = {
+    attach: function (context) {
+
+      
+    }
+  };
+})(jQuery);
