c48b7edbc04f30f9e41c01eaa81f034fa2093d63
Nesta <nguerrero@emergya.com>
2017-09-21 12:21:20 +0200

    Validation

--- js/in-view-home.js
+++ js/in-view-home.js
@@ -1,44 +1,21 @@
+/**
+ * @file
+ * Custom Code for Home Section.
+ */
+
 (function ($) {
 
   var $window = $(window);
 
   function check_if_in_view() {
 
-    // Think globally, act locally
-    $('#block-homethinkgloballyactlocally:in-viewport').addClass('in-view');
-    $('#block-homethinkgloballyactlocally:below-the-fold').removeClass('in-view');
+    // General Elements.
 
-    // Discover how we can help.
-    $('#block-views-block-view-business-lines-blk-display-whatwedo-2:in-viewport').addClass('in-view');
-    $('#block-views-block-view-business-lines-blk-display-whatwedo-2:below-the-fold').removeClass('in-view');
-
-    // Some sucessful clients.
-    $('#block-views-block-view-client-blk-display-someofourclients-home:in-viewport').addClass('in-view');
-    $('#block-views-block-view-client-blk-display-someofourclients-home:below-the-fold').removeClass('in-view');
-
-    // Featured content.
-    $('#block-homefeaturedcontent:in-viewport').addClass('in-view');
-    $('#block-homefeaturedcontent:below-the-fold').removeClass('in-view');
-
-    // Stories.
-    $('#block-homeenstories:in-viewport').addClass('in-view');
-    $('#block-homeenstories:below-the-fold').removeClass('in-view');
-    $('#block-homeesstories:in-viewport').addClass('in-view');
-    $('#block-homeesstories:below-the-fold').removeClass('in-view');
-    $('#block-homeclstories:in-viewport').addClass('in-view');
-    $('#block-homeclstories:below-the-fold').removeClass('in-view');
-
-    // Join.
-    $('#block-views-block-careers-block-home:in-viewport').addClass('in-view');
-    $('#block-views-block-careers-block-home:below-the-fold').removeClass('in-view');
-
-    //General Elements.
-
-    // Contact Us
+    // Contact Us.
     $('.region-content-bottom:in-viewport').addClass('in-view');
     $('.region-content-bottom:below-the-fold').removeClass('in-view');
 
-    // Footer
+    // Footer.
     $('footer:in-viewport').addClass('in-view');
     $('footer:below-the-fold').removeClass('in-view');
 
